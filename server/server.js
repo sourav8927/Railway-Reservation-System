888e4a3ae631e3c1c71b002fc3ef0663a3eb289a
SOURAV KARMAKAR <98399593+sourav8927@users.noreply.github.com>
2024-08-03 18:10:10 +0530

    Port added for deploy

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use("/api/trainBooking",bookTrain);
 //calling error middleware
 app.use(errorMiddleware);
 
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 const hostname='127.0.0.2';
 
 connectDb().then(()=>{
